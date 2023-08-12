1809d17116443721864e458b1cda52e07d1d92b4
nAgIbAtOr1337 <7770hunter0777@gmail.com>
2023-08-12 17:04:14 +0800

    init spring boot app using spring initializer, add readme, .gitignore, ci pipeline, docker-compose, postgress

---
+++ src/test/java/com/example/Trello/TrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
